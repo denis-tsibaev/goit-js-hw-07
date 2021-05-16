bd2f8be5f5daf357ecf9c66f590a4e48c5a78d60
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-05-16 17:58:53 +0700

    Задачи дз

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,10 @@
-const totalLi = document.querySelectorAll('#categories li.item');
-console.log(totalLi);
+const totalCategories = document.querySelectorAll('#categories li.item');
+console.log('В списке категорий', totalCategories);
 
-const menuItemsNames = document.querySelectorAll('#categories li.item h2');
-console.log(menuItemsNames);
+const menuCategoryIitle = document.querySelectorAll('#categories li.item h2');
+console.log(menuCategoryIitle);
+console.log(menuCategoryIitle.textContent);
+
+const quantityItems = document.querySelectorAll('.item li');
+console.log(quantityItems);
+
